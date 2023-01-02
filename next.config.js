27f9b5ecfd9a00bcfe1b451ae67d4f77ca0f86f5
AJMikalauskas <97716972+AJMikalauskas@users.noreply.github.com>
2023-01-02 15:06:13 -0600

    Added to next config file to see if it would fix 504 error when deploying to vercel

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  experimental: {
+    forceSwcTransforms: true,
+  }
   // env: {
   //   mongodburl: "mongodb+srv://Xander_Mik:Isaiah558@cluster0.8stfsj5.mongodb.net/DynamicResumeDB?retryWrites=true&w=majority"
   // }
